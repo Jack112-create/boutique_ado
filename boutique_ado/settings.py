adb636723e66700047d3b58b196443b272ad07a7
Jack Conroy <jackconroy777@gmail.com>
2022-07-28 20:22:03 +0000

    Fix database variable in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
